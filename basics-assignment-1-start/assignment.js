a768bbc98a03916dfc6a2400a95081952957d072
AlbySaji <albysajim@gmail.com>
2023-07-05 16:00:36 +0530

    second commit

--- basics-assignment-1-start/assignment.js
+++ basics-assignment-1-start/assignment.js
@@ -1,8 +1,8 @@
-const userInput = 1000
-let result
-result = 18+userInput
+const userInput = 1000;
+let result;
+result = 18+userInput;
 
-result = result*10;
+result = result*19;
 result = result-10;
 result = result/2
 
